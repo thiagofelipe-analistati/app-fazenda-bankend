fd128d886664ddfe87415cade652d3b2f9a9e2a8
Thiago felipe <thiagofelipe_analistati@outlook.com>
2021-11-04 14:10:14 -0300

    Projeto criado

---
+++ src/main/java/com/devthiago/appfazenda/AppFazendaApplication.java
@@ -0,0 +1,13 @@
+package com.devthiago.appfazenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFazendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFazendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devthiago/appfazenda/AppFazendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthiago.appfazenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFazendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
