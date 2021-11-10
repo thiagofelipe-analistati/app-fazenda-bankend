07eae9a68e262e697ac75650052c9659e1de103c
Thiago felipe <thiagofelipe_analistati@outlook.com>
2021-11-10 12:39:48 -0300

    Profile dev

--- src/main/java/com/devthiago/appfazenda/service/ProprietariosService.java
+++ src/main/java/com/devthiago/appfazenda/service/ProprietariosService.java
@@ -3,6 +3,8 @@ package com.devthiago.appfazenda.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -40,10 +42,13 @@ public class ProprietariosService {
 		}
 	}
 	public Proprietarios update(Long id, Proprietarios obj) {
+		try {
 		Proprietarios entity = userRepository.getOne(id);
 		updateData(entity, obj);
 		return userRepository.save(entity);
-		
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(Proprietarios entity, Proprietarios obj) {
 		entity.setName(obj.getName());
